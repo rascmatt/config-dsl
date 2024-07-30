97e860543ce9411f1e6572181e0c3abbdb96a8be
Matthias Raschhofer <matthias@armstrongconsulting.net>
2024-07-30 10:18:49 +0200

    Add clarity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {CdsIcon, detailsIcon} from "@cds/core/icon";
+
+CdsIcon.bind(detailsIcon);
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'config-dsl';
+  title = 'Config DSL';
 }
